e992b658cb1c1d9f9db4ea0c278bbd53dd7a9e95
liaofei <136327134@qq.com>
2022-01-07 17:13:18 +0800

    修改默认配置

--- template/uniapp/pages/config/app.js
+++ template/uniapp/pages/config/app.js
@@ -1,10 +1,10 @@
 module.exports = {
 	//默认域名,域名格式例如:chat.crmeb.net
-	defaultDomainName: 'chat.crmeb.net',
+	defaultDomainName: '',
 	//默认请求方式为https或者http
 	defaultRequestType: 'https',
 	//默认ws链接方式,连接方式wss或者ws
 	defaultWsType: 'wss',
 	//是否打开自定义配置域名,可选值:true|false;关闭后必须要配置默认域名(defaultDomainName)选项
-	isDomainName: false,
+	isDomainName: true,
 }
